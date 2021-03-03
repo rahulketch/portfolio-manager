73ec6e9e185539afdd65283a241ce031ffd0b52a
Rahul Sharma <rahul.kenu@gmail.com>
2021-03-03 19:28:28 +0100

    Refactor into a class (#4)

--- src/position_overview.py
+++ src/position_overview.py
@@ -20,59 +20,62 @@ def ticker_from_isin(isin):
         return None
 
 
-def read_degiro_transactions(filepath):
-    df = pandas.read_csv(filepath)
-    column_rename = {
-        'Datum': 'Date',
-        'Product': 'Product',
-        'ISIN': 'ISIN',
-        'Aantal': 'Volume',
-        'Koers': 'Price',
-        'Totaal': 'Invested',
-    }
-    df = df[column_rename.keys()].rename(columns=column_rename)
-    df['Invested'] = -1 * df['Invested'].fillna(0)
-    return df
+class PortfolioOverview:
+    def __init__(self, files):
+        self.eur_usd_conversion = yf.Ticker('EURUSD=X').history(period="1d")['Close'][0]
+        self.transactions = pandas.concat([self._read_degiro_transactions(file) for file in files])
+        self.positions = self._get_positions()
 
+    def _get_positions(self):
+        def _get_current_value(row):
+            if row.Volume == 0:
+                return 0
+            if pandas.isna(row.Price):
+                raise RuntimeError(f"{row.ISIN} has no price")
+            return row.Price * row.Volume
 
-def get_ticker_price(isin):
-    ticker_name = ticker_from_isin(isin)
-    if ticker_name is None:
-        return None
-    ticker = yf.Ticker(ticker_name)
-    price = ticker.history(period="1d")['Close'][0]
-    if ticker.info['currency'] == 'EUR':
-        return price
-    elif ticker.info['currency'] == 'USD':
-        return price / yf.Ticker('EURUSD=X').history(period="1d")['Close'][0]
-    else:
-        raise RuntimeError(f"Currency {ticker.info['currency']} not supported")
+        def get_ticker_price(isin):
+            ticker_name = ticker_from_isin(isin)
+            if ticker_name is None:
+                return None
+            ticker = yf.Ticker(ticker_name)
+            price = ticker.history(period="1d")['Close'][0]
+            if ticker.info['currency'] == 'EUR':
+                return price
+            elif ticker.info['currency'] == 'USD':
+                return price / self.eur_usd_conversion
+            else:
+                raise RuntimeError(f"Currency {ticker.info['currency']} not supported")
 
+        positions = self.transactions.groupby(['Product', 'ISIN']).agg({'Volume': 'sum', 'Invested': 'sum'}).reset_index()
+        positions['Price'] = positions['ISIN'].apply(get_ticker_price)
+        positions['Value'] = positions.apply(_get_current_value, axis=1)
+        positions.loc["Total"] = positions[['Value', 'Invested']].sum()
+        positions['Percent Change'] = (positions['Value'] - positions['Invested']) / positions['Invested'] * 100
+        return positions.sort_values('Value', ascending=False)
 
-def get_current_value(row):
-    if row.Volume == 0:
-        return 0
-    if pandas.isna(row.Price):
-        raise RuntimeError(f"{row.ISIN} has no price")
-    return row.Price * row.Volume
-
-
-def get_positions(df):
-    positions = df.groupby(['Product', 'ISIN']).agg({'Volume': 'sum', 'Invested': 'sum'}).reset_index()
-    positions['Price'] = positions['ISIN'].apply(get_ticker_price)
-    positions['Value'] = positions.apply(get_current_value, axis=1)
-    positions.loc["Total"] = positions[['Value', 'Invested']].sum()
-    positions['Percent Change'] = (positions['Value'] - positions['Invested']) / positions['Invested'] * 100
-    return positions
+    @staticmethod
+    def _read_degiro_transactions(filepath):
+        df = pandas.read_csv(filepath)
+        column_rename = {
+            'Datum': 'Date',
+            'Product': 'Product',
+            'ISIN': 'ISIN',
+            'Aantal': 'Volume',
+            'Koers': 'Price',
+            'Totaal': 'Invested',
+        }
+        df = df[column_rename.keys()].rename(columns=column_rename)
+        df['Invested'] = -1 * df['Invested'].fillna(0)
+        return df
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--files', nargs='+')
     args = parser.parse_args()
-    transactions = pandas.concat([read_degiro_transactions(file) for file in args.files])
-    positions = get_positions(transactions)
-    print(positions.sort_values('Value', ascending=False).to_string())
+    portfolio_overview = PortfolioOverview(args.files)
+    print(portfolio_overview.positions.to_string())
 
 
 if __name__ == "__main__":
