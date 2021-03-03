3b86663b94dcbae66aeb6432c4a945d9fcacd847
Rahul Sharma <rahul.kenu@gmail.com>
2021-03-03 18:25:19 +0100

    Do not get prices when volume is 0. Update output to display ISIN + Product Name (#2)

--- README.md
+++ README.md
@@ -10,11 +10,11 @@ The project uses `yfinance` to get the latest price of the securities.
 3. Run the script using `python src/position_overview.py -f example_transactions.csv`
 4. The output should look like below:
 ```
-        Ticker  Volume  Invested       Price         Value  Percent Change
-0      EMIM.AS   260.0   6368.99   31.455000   8178.299980       28.408115
-1      IBCI.AS    14.0   3083.95  224.539993   3143.559906        1.932908
-2      IWDA.AS   810.0  42213.66   62.380001  50527.800865       19.695380
-3      TKWY.AS     0.0    -86.41   77.739998      0.000000     -100.000000
-4      TPSA.AS    13.0   2600.88  199.199997   2589.599960       -0.433701
-Total      NaN     NaN  54181.07         NaN  64439.260712       18.933164
+                  Product          ISIN  Volume  Invested       Price        Value  Percent Change
+Total                 NaN           NaN     NaN   7159.72         NaN  8295.324963       15.861025
+2      ISHARES MSCI WOR A  IE00B4L5Y983   119.0   6500.16   62.535000  7441.664982       14.484335
+1            ISHARES EMIM  IE00BKM4GZ66    20.0    526.06   31.462999   629.259987       19.617532
+0      ISHARES E INF-LINK  IE00B0M62X26     1.0    219.91  224.399994   224.399994        2.041742
+3                TAKEAWAY  NL0012015705     0.0    -86.41         NaN     0.000000     -100.000000
+
 ```
--- src/position_overview.py
+++ src/position_overview.py
@@ -2,42 +2,60 @@ import pandas
 import yfinance as yf
 import argparse
 
+
 def ticker_from_isin(isin):
-    return {
+    mapping = {
         'IE00B4L5Y983': 'IWDA.AS',
         'IE00B0M62X26': 'IBCI.AS',
         'IE00B1FZSC47': 'TPSA.AS',
         'IE00BKM4GZ66': 'EMIM.AS',
-        'NL0012015705': 'TKWY.AS'
-    }[isin]
+        'US21833P1030': 'CRBP',
+        'US2210151005': 'CRVS',
+        'US39874R1014': 'GO'
+    }
+
+    if isin in mapping:
+        return mapping[isin]
+    else:
+        return None
 
 
 def read_degiro_transactions(filepath):
     df = pandas.read_csv(filepath)
     column_rename = {
         'Datum': 'Date',
-        'ISIN': 'Ticker',
+        'Product': 'Product',
+        'ISIN': 'ISIN',
         'Aantal': 'Volume',
         'Koers': 'Price',
-        'Totaal': 'Invested'
+        'Totaal': 'Invested',
     }
     df = df[column_rename.keys()].rename(columns=column_rename)
     df['Invested'] = -1 * df['Invested'].fillna(0)
-    df['Ticker'] = df['Ticker'].apply(ticker_from_isin)
     return df
 
 
-def get_ticker_price(ticker):
+def get_ticker_price(isin):
+    ticker = ticker_from_isin(isin)
+    if ticker is None:
+        return None
     return yf.Ticker(ticker).history(period="1d")['Close'][0]
 
 
+def get_current_value(row):
+    if row.Volume == 0:
+        return 0
+    if pandas.isna(row.Price):
+        raise RuntimeError(f"{row.ISIN} has no price")
+    return row.Price * row.Volume
+
+
 def get_positions(df):
-    positions = df.groupby('Ticker').agg({'Volume': 'sum', 'Invested': 'sum'}).reset_index()
-    positions['Price'] = positions['Ticker'].apply(get_ticker_price)
-    positions['Value'] = positions['Volume'] * positions['Price']
+    positions = df.groupby(['Product', 'ISIN']).agg({'Volume': 'sum', 'Invested': 'sum'}).reset_index()
+    positions['Price'] = positions['ISIN'].apply(get_ticker_price)
+    positions['Value'] = positions.apply(get_current_value, axis=1)
     positions.loc["Total"] = positions[['Value', 'Invested']].sum()
     positions['Percent Change'] = (positions['Value'] - positions['Invested']) / positions['Invested'] * 100
-
     return positions
 
 
@@ -47,7 +65,7 @@ def main():
     args = parser.parse_args()
     transactions = pandas.concat([read_degiro_transactions(file) for file in args.files])
     positions = get_positions(transactions)
-    print(positions)
+    print(positions.sort_values('Value', ascending=False).to_string())
 
 
 if __name__ == "__main__":
